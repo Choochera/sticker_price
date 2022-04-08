259a0a3415f6f7e279d325db04400c4265d37fb7
Choochera <gabrielmatthew20@gmail.com>
2022-04-08 18:40:21 -0500

    Corrected algorithm error/multiple fixes

--- sticker_price.py
+++ sticker_price.py
@@ -1,5 +1,4 @@
 import json
-from multiprocessing.sharedctypes import Value
 import requests
 import pandas as pd
 import numpy as np
@@ -10,32 +9,71 @@ import statistics
 import requests
 import lxml.html as lh
 import os
+from currency_converter import CurrencyConverter
 
-#Template for later API calls
+# Rule 1 equation application. Uses PE and equity growth to predict what price will be based on required rate of return and number of years.
+
 def retrieve_data(function: str, symbol: str, api_key: str) -> dict:
     url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={api_key}'
-    response = requests.get(url)
-    data = response.text
-    return json.loads(data)
+    try:
+        response = requests.get(url)
+        data = response.text
+    except requests.exceptions.HTTPError as hError:
+        raise Exception(str("Http Error:", hError))
+    except requests.exceptions.ConnectionError as cError:
+        raise Exception(str("Error Connecting:", cError))
+    except requests.exceptions.Timeout as tError:
+        raise Exception(str("Timeout Error:", tError))
+    except requests.exceptions.RequestException as rError:
+        raise Exception(str("Other Error:", rError))
+
+    #Make limited attempts to retrieve data from API
+    attempts = 0
+    while(attempts <= 3):
+        try: 
+            json.loads(data)['Note']
+            attempts += 1
+            time.sleep(3) #log message 
+        except KeyError:
+            return json.loads(data)
+
+    #In even of exceeded API use, HTTP error is thrown
+    raise Exception('Total API Calls Exceeded - please wait and try again')
 
 def retrieve_balance_sheets(symbol: str, api_key: str):
-    balance_sheets = retrieve_data('BALANCE_SHEET', symbol, api_key)['quarterlyReports']
-    return balance_sheets
+    try:
+        result = retrieve_data('BALANCE_SHEET', symbol, api_key)['quarterlyReports']
+    except KeyError:
+        raise KeyError(str(symbol) + " is not a valid symbol!")
+    return result
 
 def retrieve_current_EPS(symbol: str, api_key: str):
+    quarterly_EPS = retrieve_data('EARNINGS', symbol, api_key)
+
+    #Isolate the last four quarters (or less if that is unavailable)
+    if(len(quarterly_EPS) > 4):
+        quarterly_EPS = quarterly_EPS['quarterlyEarnings'][0:4]
+    else: 
+        quarterly_EPS = quarterly_EPS['quarterlyEarnings'][0 : len(quarterly_EPS)]
+
+    #Accumulate EPS for the passed four quarterly earnings
     current_eps = 0
-    quarterly_EPS = retrieve_data('EARNINGS', symbol, api_key)['quarterlyEarnings'][0:4]
     for quarter in quarterly_EPS:
-        current_eps += round(float(quarter['reportedEPS']), 2)
+        current_eps += round(float(quarter['reportedEPS']), 3)
+
     return current_eps
 
 def retrieve_annual_PE(symbol: str, api_key: str, num_years: int):
+
+    # ttm PE = price at earnings announcement / ttm EPS
     quarterly_EPS = retrieve_data('EARNINGS', symbol, api_key)['quarterlyEarnings']
     annual_PE = []
 
+    # Calculating ttm EPS for each respective annual PE
+    # Start at earliest report date
     i = 0
-    while i < len(quarterly_EPS) and i < num_years * 4:
-        h_data = yf.download(symbol, quarterly_EPS[i]['fiscalDateEnding'])['Close']
+    while i + 4 < len(quarterly_EPS) and i < num_years * 4:
+        h_data = yf.download(symbol, quarterly_EPS[i]['reportedDate'])['Close']
         price = float(h_data[0])
         try:
             eps = float(quarterly_EPS[i]['reportedEPS']) + float(quarterly_EPS[i + 1]['reportedEPS']) + float(quarterly_EPS[i + 2]['reportedEPS']) + float(quarterly_EPS[i + 3]['reportedEPS'])
@@ -49,15 +87,15 @@ def retrieve_annual_PE(symbol: str, api_key: str, num_years: int):
                     try:
                         eps = float(quarterly_EPS[i]['reportedEPS'])
                     except:
-                        print("This shouldn't be possible")
+                        raise Exception("Error: This Symbol has no reported EPS")
 
-        print(str(quarterly_EPS[i]['fiscalDateEnding']) + " --- " + str(price) + " --- " + str(eps))
+        print(str(quarterly_EPS[i]['reportedDate']) + " --- " + str(price) + " --- " + str(eps))
         annual_PE.append(price/eps)
-        i += 4
+        i += 4 #Move on to next set of 4 quarters (or 1 year)
 
     if( len(annual_PE) != num_years ):
             eps = float(quarterly_EPS[len(quarterly_EPS) - 1]['reportedEPS'])
-            h_data = yf.download(symbol, quarterly_EPS[len(quarterly_EPS) - 1]['fiscalDateEnding'])['Close']
+            h_data = yf.download(symbol, quarterly_EPS[len(quarterly_EPS) - 1]['reportedDate'])['Close']
             price = float(h_data[0])
             annual_PE.append(price/eps)
 
@@ -66,20 +104,37 @@ def retrieve_annual_PE(symbol: str, api_key: str, num_years: int):
 def retrieve_quarterly_BVPS(symbol :str, api_key: str):
     quarterly_BVPS = []
     balance_sheets = retrieve_balance_sheets(symbol, api_key)
+
+    c = CurrencyConverter()
+    currency = 'USD'
+    
     if(balance_sheets[0]['reportedCurrency'] != 'USD'):
-        print('These values are supplied in: ' + str(balance_sheets[0]['reportedCurrency']))
+        currency = balance_sheets[0]['reportedCurrency']
 
     for quarter in balance_sheets:
         try:
-            quarterly_BVPS.append(round(float(quarter['totalShareholderEquity'])/float(quarter['commonStockSharesOutstanding']), 3))
+            if( float(quarter['totalShareholderEquity']) < 0): raise Exception ("Negative BVPS within the passed 5 years - not a rule 1 company")
+            quarterly_BVPS.append(round(float(c.convert(quarter['totalShareholderEquity'], currency, 'USD'))/float(quarter['commonStockSharesOutstanding']), 3))
         except ValueError:
-            print(quarter['fiscalDateEnding'] + " Skipped")    
+            print(quarter['fiscalDateEnding'] + " Skipped")   
+
     return quarterly_BVPS
 
 def retrieve_fy_growth_estimate(symbol: str):
     url = "https://www.zacks.com/stock/quote/" + symbol + "/detailed-estimates"
-    page = requests.get(url, headers = {'User-Agent' : '008'})
-    doc = lh.fromstring(page.content)
+
+    try:
+        page = requests.get(url, headers = {'User-Agent' : '008'})
+        doc = lh.fromstring(page.content)
+    except requests.exceptions.HTTPError as hError:
+        raise Exception(str("Http Error:", hError))
+    except requests.exceptions.ConnectionError as cError:
+        raise Exception(str("Error Connecting:", cError))
+    except requests.exceptions.Timeout as tError:
+        raise Exception(str("Timeout Error:", tError))
+    except requests.exceptions.RequestException as rError:
+        raise Exception(str("Other Error:", rError))
+
     td_elements = doc.xpath('//td')
 
     for i in range(len(td_elements)):
@@ -90,13 +145,11 @@ def retrieve_fy_growth_estimate(symbol: str):
 
 if __name__ == '__main__':
 
+    #Access OS environmental variable alpha key, or save a new one
     try:
         api_key = os.getenv('ALPHA_API_KEY')
     except:
         os.environ['ALPHA_API_KEY'] = input("Insert alpha vantage API key: ")
-
-    num_years = 10
-    percent_return = 15
 
     #read in symbol to be process
     symbol = sys.argv[1]
@@ -105,71 +158,69 @@ if __name__ == '__main__':
     data = dict()
     annual_BVPS = []
 
-    #retrieve quarterly book value per share data
-    while(True):
+    try:    
+        #retrieve quarterly book value per share data
+        quarterly_BVPS = retrieve_quarterly_BVPS(symbol, api_key)
+
+        #annualize quarterly book values for dataframe consistency
+        i = 0
+        while i < len(quarterly_BVPS) and i < 20:
+            annual_BVPS.append(statistics.mean(quarterly_BVPS[i: i+4]))
+            i += 4
+
+        #Add Book Value per share annualized values to dataframe
+        data['BVPS'] = annual_BVPS
+        
+        # Calculate trailing 5 year annual BVPS growth rate
         try:
-            quarterly_BVPS = retrieve_quarterly_BVPS(symbol, api_key)
-            break
-        except KeyError:
-            time.sleep(3)
+            tfy_BVPS_growth = ( ( annual_BVPS[0]/annual_BVPS[len(annual_BVPS) - 1] ) ** ( 1/len(annual_BVPS) ) - 1 ) * 100
+            if( tfy_BVPS_growth < 0 ): raise Exception("Equity growth rate is negative - not a rule 1 company")
+        except IndexError:
+            raise Exception("Not enough historical data available for symbol: " + symbol)
 
-    #annualize this data for dataframe
-    i = 0
-    while i < len(quarterly_BVPS) and i < 20:
-        annual_BVPS.append(statistics.mean(quarterly_BVPS[i: i+4]))
-        i += 4
+        # Retrieve current EPS and set values for equation
+        data['PE'] = retrieve_annual_PE(symbol, api_key, len(data['BVPS']))
+        current_eps = retrieve_current_EPS(symbol, api_key)
+        equity_growth_rate = tfy_BVPS_growth
+        forward_PE = sum(data['PE'])/5
 
-    #add Book Value per share annualized values to dataframe
-    data['BVPS'] = annual_BVPS
-    
-    #calculate twelve month BVPS for 1 year and trailing five month for 5 year
-    ttm_BVPS_growth = ((quarterly_BVPS[0]/quarterly_BVPS[3]) ** (1) - 1)*100
-    tfy_BVPS_growth = ((quarterly_BVPS[0]/quarterly_BVPS[len(quarterly_BVPS) - 1]) ** (1/(len(quarterly_BVPS)/4)) - 1)*100
-
-    #Retrieve current EPS and set values for equation
-    while(True):
-        try:
-            data['PE'] = retrieve_annual_PE(symbol, api_key, len(data['BVPS']))
-            current_eps = retrieve_current_EPS(symbol, api_key)
-            equity_growth_rate = tfy_BVPS_growth
-            forward_PE = sum(data['PE'])/5
-            break
-        except KeyError:
-            time.sleep(3)
-    try:
         df = pd.DataFrame(data, columns = ['BVPS', 'PE'])
-    except ValueError:
-        print(len(data['BVPS']))
-        print(len(data['PE']))
-    print(df)
+        print(df)
 
-    #If estimates are less than predicted, go with the estimates ( Future Price/Earnings and Future growth rate)
-    if forward_PE > equity_growth_rate.real * 2:
-        forward_PE = equity_growth_rate * 2
+        # If estimates are less than predicted, go with the estimates ( Future Price/Earnings and Future growth rate )
+        if forward_PE > equity_growth_rate * 2:
+            forward_PE = equity_growth_rate * 2
 
-    try:
-        analyst_growth_estimate = float(retrieve_fy_growth_estimate(symbol))
-    except ValueError:
-        print("No analyst growth rate available")
-        analyst_growth_estimate = equity_growth_rate.real
+        try:
+            analyst_growth_estimate = float(retrieve_fy_growth_estimate(symbol))
+        except ValueError:
+            print("No analyst growth rate available")
+            analyst_growth_estimate = equity_growth_rate.real
 
-    if equity_growth_rate.real > analyst_growth_estimate.real:
-        equity_growth_rate = analyst_growth_estimate.real
+        # If analyst estimates are lower than predicted equity growth rate, go with them
+        if equity_growth_rate > analyst_growth_estimate.real:
+            equity_growth_rate = analyst_growth_estimate.real
 
-    print('Current EPS: ', current_eps)
-    print('Average equity growth rate: ', equity_growth_rate)
-    print('Estimated future PE: ', forward_PE)
+        print('Current EPS: ', current_eps)
+        print('Average equity growth rate: ', equity_growth_rate)
+        print('Estimated future PE: ', forward_PE)
 
+        # Calculate the sticker price of the stock today relative to what predicted price will be in the future
+        num_years = 10
+        percent_return = 15
 
-    #calculate the sticker price of the stock today relative to what predicted price will be in the future
-    time_to_double = np.log(2)/np.log(1 + (equity_growth_rate/100))
-    num_of_doubles = num_years/time_to_double
-    future_price = forward_PE * current_eps * 2 ** num_of_doubles
+        # Plug in acquired values into Rule #1 equation
+        time_to_double = np.log(2)/np.log(1 + (equity_growth_rate/100))
+        num_of_doubles = num_years/time_to_double
+        future_price = forward_PE * current_eps * 2 ** num_of_doubles
+        
+        return_time_to_double = np.log(2)/np.log(1 + (percent_return/100))
+        number_of_equity_doubles = num_years/return_time_to_double
+        print(2 ** number_of_equity_doubles)
+        sticker_price = future_price/( 2 ** number_of_equity_doubles )
+
+        print("Sticker Price:  $", sticker_price)
+        print("On-Sale Price: $", sticker_price/2)
     
-    return_time_to_double = np.log(2)/np.log(1 + (percent_return/100))
-    number_of_equity_doubles = num_years/return_time_to_double
-    print(2 ** number_of_equity_doubles)
-    sticker_price = future_price/( 2 ** number_of_equity_doubles )
-
-    print("Sticker Price:  $", sticker_price)
-    print("On-Sale Price: $", sticker_price/2)
+    except Exception as e:
+        print(str(e))
